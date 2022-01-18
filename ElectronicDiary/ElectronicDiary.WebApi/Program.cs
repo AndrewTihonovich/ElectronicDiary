562c38d64bf41c0a58d0ce38772cf886c8781aa9
AndrewTihonovich <AndrewT_87@tut.by>
2022-01-18 22:42:07 +0300

    Serilog_Add base implementation

--- ElectronicDiary/ElectronicDiary.WebApi/Program.cs
+++ ElectronicDiary/ElectronicDiary.WebApi/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
 
 namespace ElectronicDiary.WebApi
 {
@@ -7,11 +10,33 @@ namespace ElectronicDiary.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.Console()
+               .CreateLogger();
+            
+            Log.Information("Starting host");
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration)=>configuration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+                .WriteTo.Console())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
