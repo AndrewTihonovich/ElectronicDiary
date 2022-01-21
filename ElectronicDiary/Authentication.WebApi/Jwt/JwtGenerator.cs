2b43f1e925f1de3f4c5aef5b8d3a90e8080b1f34
AndrewTihonovich <AndrewT_87@tut.by>
2022-01-21 22:19:49 +0300

    Authentication_Add jwt generator

---
+++ ElectronicDiary/Authentication.WebApi/Jwt/JwtGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Authentication.WebApi.User;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.WebApi.Jwt
+{
+    public class JwtGenerator : IJwtGenerator
+    {
+        private readonly SymmetricSecurityKey _key;
+
+        public JwtGenerator(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("a900fae1-70ed-4698-9953-3a247c104366"));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Login),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email) };
+
+            var credentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddHours(3),
+                SigningCredentials = credentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
