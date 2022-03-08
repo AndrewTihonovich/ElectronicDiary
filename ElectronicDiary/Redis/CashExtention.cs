fdaad6e687681946308872fbe07ee365e5fe1c73
Andrew Tihonovich <AndrewT_87@tut.by>
2022-03-08 22:17:55 +0300

    Redis_Add extentiom methode for cach

---
+++ ElectronicDiary/Redis/CashExtention.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Redis
+{
+    public static class CashExtention
+    {
+        public static async Task SetCashItemAsync<T>(this IDistributedCache cache, string key, T data)
+        {
+            TimeSpan? absoluteExpireTime = null;
+            TimeSpan? unusedExpireTime = null;
+
+            var option = new DistributedCacheEntryOptions();
+
+            option.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromMinutes(5);
+            option.SlidingExpiration = unusedExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(key, jsonData, option);
+        }
+
+        public static async Task<T> GetCashItemAsync<T>(this IDistributedCache cache, string key)
+        {
+            var jsonData = await cache.GetStringAsync(key);
+
+            if (jsonData is null)
+            {
+                return default(T);
+            }
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
